22a41dfea84830eb19b8ae0921b746095002c5eb
jypxreeya <jayapriyakalidas@gmail.com>
2025-10-22 13:42:35 +0530

    You leapt from crumbling bridges

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,21 @@
 import { Header } from '../components/Header';
-import { products } from '../../Starting-code/data/products';
-
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 import './HomePage.css';
 
 export function HomePage() {
-
+  const [products,setProducts] =useState([]);
+  useEffect(( ) => {
+         axios.get( 'http://localhost:3000/api/products')
+    .then((response ) =>{
+        setProducts(response.data);
+    });
+  },[]);
+    
   
   return (
     <>
-      <title>Ecommerce Project</title>
+      <title>Ecommerce Project</title>  
 
       <Header />
 
