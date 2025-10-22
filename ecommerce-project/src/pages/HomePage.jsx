a8aa097650d030b0678a97338da86345853aeeb5
jypxreeya <jayapriyakalidas@gmail.com>
2025-10-22 15:02:20 +0530

    You've been locked in here forever,and you just can't say goodbye

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/Header';
 import { useEffect,useState } from 'react';
+import {formatMoney } from './utils/money';
 import axios from 'axios';
 import './HomePage.css';
 
@@ -46,7 +47,7 @@ export function HomePage({cart}) {
             </div>
 
             <div className="product-price">
-             ${product.priceCents/100}
+             {formatMoney(product.priceCents)}
             </div>
 
             <div className="product-quantity-container">
