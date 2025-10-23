206f64c2e31a5b1c5ba44ad48a3880b103cc2159
jypxreeya <jayapriyakalidas@gmail.com>
2025-10-23 17:09:53 +0530

    Oh, please

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -8,13 +8,12 @@ export function HomePage({cart}) {
   const [products,setProducts] =useState([]);
   
   useEffect(( ) => {
-         axios.get( '/api/products')
-    .then((response ) =>{
-      console.log(response.data);
-        setProducts(response.data);
-    });
+    const getHomeData = async () => {
+          const response = await axios.get( '/api/products');
+   setProducts(response.data);
 
-    
+    };
+    getHomeData();
   },[]);
     
   
